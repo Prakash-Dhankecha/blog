81e0aeaac1206fb24ba3b0f2ef4295a6a107ddb3
Prakash Dhankecha <pdhankecha232@rku.ac.in>
2024-03-17 11:34:17 +0530

    remove vercel configuration

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1)mdce7md6f&p_$t74!g8_x)-hozoa-^*t358kn1b)onlqq$_8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
